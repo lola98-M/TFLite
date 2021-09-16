8693a819497967cad233061fbef980b25955c205
lola98-M <61836456+lola98-M@users.noreply.github.com>
2021-09-16 15:44:14 +0200

    Update inf.cpp

--- inf.cpp
+++ inf.cpp
@@ -33,7 +33,7 @@ int main(){
     }
         
     // read image file
-    cv::Mat img = cv::imread("sneakers.jpg");
+    cv::Mat img = cv::imread("5.jpg");
 
     cv::Mat inputImg;
     img.convertTo(inputImg, CV_32FC3);
@@ -50,7 +50,7 @@ int main(){
 
     // create model
     std::unique_ptr<tflite::FlatBufferModel> model =
-        tflite::FlatBufferModel::BuildFromFile("TensorflowLite_FashionMnist.tflite");
+        tflite::FlatBufferModel::BuildFromFile("tf_mnist.tflite");
         
     tflite::ops::builtin::BuiltinOpResolver resolver;
     std::unique_ptr<tflite::Interpreter> interpreter;
